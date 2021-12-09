498079ed7a3c1bdfc3e70d03143cb5f2e275a692
Bryce Slade <bryceslade@gmail.com>
2021-12-09 09:09:46 -0700

    two tests not passing

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,6 +1,5 @@
-const db = require('../../data/db-config')
-
-/* 
+const Scheme = require("./scheme-model");
+/*
   If `scheme_id` does not exist in the database:
 
   status 404
@@ -9,20 +8,17 @@ const db = require('../../data/db-config')
   }
 */
 const checkSchemeId = async (req, res, next) => {
+  const { scheme_id } = req.params;
+  const scheme = await Scheme.findById(scheme_id);
   try {
-    const existing = await db('schemes')
-      .where('scheme_id,', req.params.scheme_id)
-      .first()
-
-      if (!existing) {
-        next({ status: 404, message: `scheme with scheme_id ${req.params.scheme_id} not found`})
-      }
-      else {
-        next()
-      }
-  }
-  catch (err) {
-    next(err)
+    if (!scheme_id) {
+      next( { status: 404, message: `scheme with scheme_id ${scheme_id} not found` } )
+    } else {
+      req.scheme = scheme;
+      next();
+    }
+  } catch (error) {
+    next(error);
   }
 }
 
@@ -34,13 +30,19 @@ const checkSchemeId = async (req, res, next) => {
     "message": "invalid scheme_name"
   }
 */
-const validateScheme = (req, res, next) => {
-  const { scheme_name } = req.body
-  if (scheme_name === undefined || typeof scheme_name !== 'string' || scheme_name.trim()) {
-    next({ status: 400, message: 'invalid scheme_name'})
-  }
-  else {
-    next()
+const validateScheme = async (req, res, next) => {
+  try {
+    const { scheme_name } = req.body
+    
+    if ( !scheme_name || typeof scheme_name !== "string" ) {
+      next( { status: 400, message: "invalid scheme_name" } );
+
+    } else {
+      next();
+    }
+
+  } catch (error) {
+    next(error);
   }
 }
 
@@ -53,15 +55,20 @@ const validateScheme = (req, res, next) => {
     "message": "invalid step"
   }
 */
-const validateStep = (req, res, next) => {
-  const { instructions, step_number } = req.body
-  
-  if (instructions === undefined || typeof instructions !== 'string' || instructions.trim() || typeof step_number !== 'number' || step_number < 1) {
-    const error = { status: 400, message: 'invalid step' }
-    next(error)
-  }
-  else {
-    next()
+const validateStep = async (req, res, next) => {
+  try {
+    const { instructions, step_number } = req.body
+
+    if( !instructions || instructions === "" || typeof instructions !== "string" ||
+      step_number < 1 || typeof step_number !== "number") {
+        next( { status: 400, message: "invalid step" } )
+
+    } else {
+      next();
+    }
+
+  } catch (error) {
+    next(error);
   }
 }
 
